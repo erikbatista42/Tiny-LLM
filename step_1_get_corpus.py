72cdea9ab3ef9652bc8374fb1c239faa1bec5bd4
Erik Batista <erikbatista42@gmail.com>
2025-10-24 12:28:21 -0400

    added first step

---
+++ step_1_get_corpus.py
@@ -0,0 +1,21 @@
+import requests
+
+def download_corpus():
+    # tiny Shakespeare dataset that is already cleaned.
+    url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+
+    # download the text
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Save the content to a file
+        with open("input.txt", "w", encoding="utf-8") as file:
+            file.write(response.text)
+        text = response.text
+        print("File downloaded successfully.")
+        print("📊 Corpus size: ", len(text), "characters")
+        print(f"👀 Preview of first 200 characters:\n{text[:200]}...")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+download_corpus()
