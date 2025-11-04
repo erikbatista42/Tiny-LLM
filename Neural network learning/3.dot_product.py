f50acb0b20ed735e6487023840c6adc0a14c1640
erikbatista42 <erikbatista42@gmail.com>
2025-11-04 08:55:42 -0500

    added learnings from linear regression

--- Neural network learning/3.dot_product.py
+++ Neural network learning/3.dot_product.py
@@ -1,22 +1,9 @@
-#  REPRESENTING A hidden layer of a neural network.
+import numpy as np
 
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
 
-inputs = [1, 2, 3, 2.5] # each input is a neuron
+bias = 2
+output = np.dot(weights, inputs) + bias
 
-# each neuron has its own weights
-weights1 = [0.2, 0.8, -0.5, 1.0]
-weights2 = [0.5, -0.91, 0.26, -0.5]
-weights3 = [-0.26, -0.27, 0.17, 0.87]
-
-weights = [weights1, weights2, weights3]
-
-# This value comes from the neuron itself. It does not come from other neurons.
-bias1 = 2
-bias2 = 3
-bias3 = 0.5
-
-biases = [bias1, bias2, bias3]
-
-output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
-          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
-          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3,
+print(output)
