ec7510fec036cbb22b961b17fd2dd5f154dff0c1
erikbatista42 <erikbatista42@gmail.com>
2025-11-07 01:11:44 -0500

    learning rectified linear unit

--- Neural network learning/5.hidden_activation_functions.py
+++ Neural network learning/5.hidden_activation_functions.py
@@ -1,8 +1,9 @@
 '''
-ACTIVATION FUNCTION
-For an activation function, you can use a variety of different functions. To start with, we're just going to look at the step function.
+**ACTIVATION FUNCTIONS**
+For an activation function, you can use a variety of different functions. 
 
 STEP FUNCTION:
+To start with, we're just going to look at the step function.
 The idea is that if your input to this function is greather than 0, then your output will be 1. Otherwise your output is going to be a 0.
 
 Now lets use this step function as an ACTIVATION function.
@@ -12,7 +13,9 @@ The main takeaway here is that the output is literally a 0 or a 1.
 
 Now, the output that is a 0 or 1 becomes the input to the next neurons.
 
-SIGMOID ACTIVATION FUNCTION
+----------
+
+SIGMOID FUNCTION
 Context: Now, while you can train a neural network with the step function, it quickly became clear that using something like a sigmoid activation function is a little more easy and reliable to train a neural network due to the granularity of the output.
 
 In this context, granularity means how detailed or fine-grained the output values can be.
@@ -21,4 +24,20 @@ This smoothness gives the neural network more nuanced information during trainin
 
 We use the sigmoid activation function after we do the inputs*weights + bias.
 
+----------
+
+RECTIFIED LINEAR UNIT FUNCTION
+This function is very simple. If input or x > 0, the output = x. If x <= 0, the output is 0.
+Like the sigmoid activation function, the output could be a little granular. It can't be less than 0 so if enough things are 0 it will output to 0.
+
+We use the rectified linear activation function after we do the inputs*weights + bias.
+
+--------------------------------------------------
+Why would we use the rectified linear function over the sigmoid function which also produces a granular ouput?
+-> The sigmoid problem has an issue reffered to as the vanishing gradient problem. 
+
+3 main reasons why we would be using rectified linear:
+    1. It's still granular so we can still optimize well with it.
+    2. It's fast because it's a very simple calculation.
+    3. It simply works.
 '''
